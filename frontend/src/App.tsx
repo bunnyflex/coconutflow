68b337ec53c06cb4810ef4ee971e66cabb93a527
Sunny <sunnypanchal99@gmail.com>
2026-02-23 16:46:10 +0000

    feat: route logged-out users to LandingPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,25 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { CanvasPage } from './pages/CanvasPage';
 import { DashboardPage } from './pages/DashboardPage';
+import { LandingPage } from './pages/LandingPage';
 import { TemplatesPage } from './pages/TemplatesPage';
 import { KeysPage } from './pages/KeysPage';
 import { DocsPage } from './pages/DocsPage';
 import { LoginPage } from './pages/LoginPage';
 import { AccountPage } from './pages/AccountPage';
 import ToastContainer from './components/ui/Toast';
+import { useAuthStore } from './store/authStore';
 
 export default function App() {
+  const user = useAuthStore((s) => s.user);
+  const loading = useAuthStore((s) => s.loading);
+
+  if (loading) return null;
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
+        <Route path="/" element={user ? <DashboardPage /> : <LandingPage />} />
         <Route path="/flow" element={<CanvasPage />} />
         <Route path="/flow/:id" element={<CanvasPage />} />
         <Route path="/templates" element={<TemplatesPage />} />
